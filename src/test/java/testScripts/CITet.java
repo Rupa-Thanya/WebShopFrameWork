6117cf8939300887153a43a4243e232e15129014
Your Name <Rupa_Thanya>
2025-04-10 15:47:10 +0530

    positive

---
+++ src/test/java/testScripts/CITet.java
@@ -0,0 +1,32 @@
+package testScripts;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import PageRepository.HomePage;
+import genericClass.BaseClass;
+
+public class CITet extends BaseClass {
+	SoftAssert soft=new SoftAssert();
+	
+	@Test()
+	public void imgSave() {
+		
+		HomePage home = new HomePage(driver);
+		
+		soft.assertEquals(true, home.getYourhouzztext().isDisplayed());
+
+		soft.assertAll();
+	}
+	
+
+	@Test(priority = 1)
+	public void faitetcae() {
+		
+		HomePage home = new HomePage(driver);
+		
+		soft.assertEquals(true, home.getYourhouzztext().isDisplayed());
+
+		soft.assertAll();
+	}
+}
